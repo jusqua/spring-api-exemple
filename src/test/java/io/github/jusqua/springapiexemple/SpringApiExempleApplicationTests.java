105cdc37c684da0a3f1390565156490b07755a11
Ádrian Gama <adrian.gama.se@gmail.com>
2024-07-24 15:28:05 -0300

    init sprint boot template

---
+++ src/test/java/io/github/jusqua/springapiexemple/SpringApiExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jusqua.springapiexemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiExempleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
